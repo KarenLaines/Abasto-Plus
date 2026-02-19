9fe50afcf246caf4a87617fc1ea5c3f584cd880d
DanielJuarez1645122 <eduardojuarez2602@gmail.com>
2026-02-19 09:21:20 -0600

    feat(VO): Se crearon los value objects del la clase presentation

---
+++ src/shared/domain/entities/presentation/presentation-name.ts
@@ -0,0 +1,7 @@
+import { StringValueObject } from "../../../../sharecl/domain/value-objects/string-value-object";
+
+export class PresentationName extends StringValueObject {
+  constructor(value: string) {
+    super(value);
+  }
+}
